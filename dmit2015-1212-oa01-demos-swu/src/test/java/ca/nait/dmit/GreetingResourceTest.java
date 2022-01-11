8756e280e9defa0b4f9751f3c403405a83f07bda
Sam Wu <swu@nait.ca>
2022-01-11 08:54:00 -0700

    first commit

---
+++ dmit2015-1212-oa01-demos-swu/src/test/java/ca/nait/dmit/GreetingResourceTest.java
@@ -0,0 +1,92 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+package ca.nait.dmit;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(ArquillianExtension.class)
+public class GreetingResourceTest {
+
+    private final static Logger LOGGER = Logger.getLogger(GreetingResourceTest.class.getName());
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addClass(GreetingMessage.class)
+                .addClass(GreetingService.class)
+                .addClasses(GreetingResource.class, JaxrsActivator.class)
+                // Enable CDI (Optional since Java EE 7.0)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @ArquillianResource
+    private URL base;
+
+    private Client client;
+
+    @BeforeEach
+    public void setup() {
+        LOGGER.info("call BeforeEach");
+        this.client = ClientBuilder.newClient();
+        //removed the Jackson json provider registry, due to OpenLiberty 21.0.0.1 switched to use Resteasy.
+    }
+
+    @AfterEach
+    public void teardown() {
+        LOGGER.info("call AfterEach");
+        if (this.client != null) {
+            this.client.close();
+        }
+    }
+
+    @Test
+    @DisplayName("Given a name: `JakartaEE` should return `Say Hello to JakartaEE`")
+    public void testGreeting() throws MalformedURLException {
+        LOGGER.log(Level.INFO, " client: {0}, baseURL: {1}", new Object[]{client, base});
+        final WebTarget greetingTarget = this.client
+                .target(new URL(this.base, "api/greeting/JakartaEE").toExternalForm());
+        try (final Response greetingGetResponse = greetingTarget.request()
+                .accept(MediaType.APPLICATION_JSON)
+                .get()) {
+            assertEquals(200, greetingGetResponse.getStatus());
+            assertTrue(greetingGetResponse.readEntity(GreetingMessage.class).getMessage().startsWith("Say Hello to JakartaEE"));
+        }
+    }
+}
---
+++ dmit2015-1212-oa01-demos-swu/src/test/java/ca/nait/dmit/GreetingServiceTest.java
@@ -0,0 +1,59 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+package ca.nait.dmit;
+
+import jakarta.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(ArquillianExtension.class)
+public class GreetingServiceTest {
+
+    private final static Logger LOGGER = Logger.getLogger(GreetingServiceTest.class.getName());
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(GreetingMessage.class)
+                .addClass(GreetingService.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    GreetingService service;
+
+    @Test
+    @DisplayName("testing buildGreetingMessage")
+    public void testBuildingGreetingMessage() {
+        LOGGER.log(Level.INFO, " Running test:: GreetingServiceTest#testBuildingGreetingMessage ... ");
+        GreetingMessage message = service.buildGreetingMessage("Jakarta EE");
+        assertTrue(message.getMessage().startsWith("Say Hello to Jakarta EE at "),
+                "message should start with \"Say Hello to Jakarta EE at \"");
+    }
+}
