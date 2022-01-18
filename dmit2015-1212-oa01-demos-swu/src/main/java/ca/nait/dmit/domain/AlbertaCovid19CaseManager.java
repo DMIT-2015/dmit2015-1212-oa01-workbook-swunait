0c3ebca6125c7779a05b394ae9948dee68d14f0e
Sam Wu <swu@nait.ca>
2022-01-18 09:58:39 -0700

    End of Jan 18 class

--- dmit2015-1212-oa01-demos-swu/src/main/java/ca/nait/dmit/domain/AlbertaCovid19CaseManager.java
+++ dmit2015-1212-oa01-demos-swu/src/main/java/ca/nait/dmit/domain/AlbertaCovid19CaseManager.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AlbertaCovid19CaseManager {
 
@@ -76,5 +77,34 @@ public class AlbertaCovid19CaseManager {
             }
         }
         return dataList;
+    }
+
+    public long countTotalActiveCases() {
+        return albertaCovid19CaseList
+                .stream()
+                .filter(item -> item.getCaseStatus().equalsIgnoreCase("Active"))
+                .count();
+    }
+
+    public long countActiveCasesByAhsZone(String ahsZone) {
+        return albertaCovid19CaseList
+                .stream()
+//                .filter(item -> item.getCaseStatus().equalsIgnoreCase("Active")
+//                    && item.getAhsZone().equalsIgnoreCase(ahsZone)
+//                )
+                .filter(item -> item.getCaseStatus().equalsIgnoreCase("Active"))
+                .filter(item -> item.getAhsZone().equalsIgnoreCase(ahsZone) )
+                .count();
+    }
+
+    public List<String> distinctAhsZones() {
+        return albertaCovid19CaseList
+                .stream()
+                .map(item -> item.getAhsZone())
+                .distinct()
+                .filter(item -> item.isEmpty() == false)
+                .sorted()
+                .collect(Collectors.toList());
+
     }
 }
--- dmit2015-1212-oa01-demos-swu/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
+++ dmit2015-1212-oa01-demos-swu/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
@@ -1,41 +1,45 @@
 package ca.nait.dmit.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlbertaCovid19CaseManagerTest {
 
-    @Test
-    void getAlbertaCovid19CaseList() throws IOException {
-//        AlbertaCovid19CaseManager caseManager = new AlbertaCovid19CaseManager();
-        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
-        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    AlbertaCovid19CaseManager caseManager;
+
+    @BeforeEach
+    void beforeEach() throws IOException {
+        caseManager = AlbertaCovid19CaseManager.getInstance();
     }
 
     @Test
-    void getAlbertaCovid19CaseList2() throws IOException {
-        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
-        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    void getAlbertaCovid19CaseList() {
+        assertEquals(436_495, caseManager.getAlbertaCovid19CaseList().size());
     }
 
     @Test
-    void getAlbertaCovid19CaseList3() throws IOException {
-        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
-        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    void activeCases() {
+        assertEquals(64_129, caseManager.countTotalActiveCases());
     }
 
     @Test
-    void getAlbertaCovid19CaseList4() throws IOException {
-        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
-        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    void activeCasesByZone()  {
+        assertEquals(29_544, caseManager.countActiveCasesByAhsZone("Calgary Zone"));
+        assertEquals(24_062, caseManager.countActiveCasesByAhsZone("Edmonton Zone"));
     }
 
     @Test
-    void getAlbertaCovid19CaseList5() throws IOException {
-        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
-        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    void distinctAhsZones() {
+        List<String> ahsZoneList = caseManager.distinctAhsZones();
+        ahsZoneList.forEach(System.out::println);
+        assertEquals(6, ahsZoneList.size());
     }
+
+
+
 }
