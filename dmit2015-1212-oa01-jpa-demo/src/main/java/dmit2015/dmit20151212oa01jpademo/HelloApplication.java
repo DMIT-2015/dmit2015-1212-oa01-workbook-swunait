dd0bcc32804ff7bd19384bf2e7f1307552cc401e
Sam Wu <swu@nait.ca>
2022-01-25 16:53:56 -0700

    added missing XML files

---
+++ dmit2015-1212-oa01-jpa-demo/src/main/java/dmit2015/dmit20151212oa01jpademo/HelloApplication.java
@@ -0,0 +1,9 @@
+package dmit2015.dmit20151212oa01jpademo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
