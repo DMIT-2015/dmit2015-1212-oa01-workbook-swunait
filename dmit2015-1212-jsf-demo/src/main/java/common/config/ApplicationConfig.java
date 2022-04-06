974952a4d0c50d763dcae44b2faba7eb712a1c87
Sam Wu <swu@nait.ca>
2022-04-06 11:51:20 -0600

    Added LDAP Auth

--- dmit2015-1212-jsf-demo/src/main/java/common/config/ApplicationConfig.java
+++ dmit2015-1212-jsf-demo/src/main/java/common/config/ApplicationConfig.java
@@ -4,6 +4,22 @@ import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.annotation.sql.DataSourceDefinitions;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.annotation.FacesConfig;
+import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.identitystore.LdapIdentityStoreDefinition;
+
+@BasicAuthenticationMechanismDefinition(
+		realmName = "jaspitest"
+)
+
+@LdapIdentityStoreDefinition(
+		url = "ldap://192.168.101.135:389",
+		callerSearchBase = "ou=Departments,dc=dmit2015,dc=ca",
+		callerNameAttribute = "SamAccountName", // SamAccountName or UserPrincipalName
+		groupSearchBase = "ou=Departments,dc=dmit2015,dc=ca",
+		bindDn = "cn=DAUSTIN,ou=IT,ou=Departments,dc=dmit2015,dc=ca",
+		bindDnPassword = "Password2015",
+		priority = 5
+)
 
 @DataSourceDefinitions({
 
--- dmit2015-1212-jsf-demo/src/main/java/dmit2015/repository/MovieRepository.java
+++ dmit2015-1212-jsf-demo/src/main/java/dmit2015/repository/MovieRepository.java
@@ -3,6 +3,8 @@ package dmit2015.repository;
 import common.jpa.AbstractJpaRepository;
 import dmit2015.entity.Movie;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.SecurityContext;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
@@ -10,6 +12,28 @@ import java.util.List;
 @ApplicationScoped
 @Transactional
 public class MovieRepository extends AbstractJpaRepository<Movie, Long> {
+
+    @Inject
+    private SecurityContext _securityContext;
+
+    public void create(Movie newMovie) {
+        String username = _securityContext.getCallerPrincipal().getName();
+        newMovie.setUsername(username);
+        super.create(newMovie);
+    }
+
+    public List<Movie> list() {
+        if (_securityContext.getCallerPrincipal() == null ) {
+            return super.list();
+        } else {
+            String username = _securityContext.getCallerPrincipal().getName();
+            return getEntityManager().createQuery(
+                            "SELECT m FROM Movie m WHERE m.username = :usernameValue "
+                            , Movie.class)
+                    .setParameter("usernameValue", username)
+                    .getResultList();
+        }
+    }
 
     public MovieRepository() {
         super(Movie.class);
