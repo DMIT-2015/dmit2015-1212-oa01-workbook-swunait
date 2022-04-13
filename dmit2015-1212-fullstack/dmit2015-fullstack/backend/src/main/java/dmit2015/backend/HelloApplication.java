f2f3b11907cbc2e295e0f147735f2be92f11af3d
Sam Wu <swu@nait.ca>
2022-04-13 10:38:11 -0600

    created fullstack project

---
+++ dmit2015-1212-fullstack/dmit2015-fullstack/backend/src/main/java/dmit2015/backend/HelloApplication.java
@@ -0,0 +1,9 @@
+package dmit2015.backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ dmit2015-1212-fullstack/dmit2015-fullstack/frontend/src/main/java/dmit2015/frontend/HelloApplication.java
@@ -0,0 +1,9 @@
+package dmit2015.frontend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
